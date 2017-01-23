53d59f19bb2dcbbad876fab1adc1aca750f4316a
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-22 19:40:45 -0700

    persona information completed and a spell check through the text was commenced.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -29,13 +29,29 @@
 
 			<!-- The following will be the persona information. the first part will contain Name, Age, Job/Role, Technology, and Needs. -->
 			<h2>Persona</h2>
-				<ul>
-					<li><strong>Name:</strong>Steven Morrissey</li>
-					<li><strong>Age:</strong>32</li>
-					<li><strong>Job:</strong>Seven Morrissey is successful custom bike shop entrepreneur who wants to start a custom bicycle online magazine that can be converted to an app for convenient viewing.</li>
-					<li><strong>Technology:</strong>Morrissey wants his online magazine to be compatible with Ipone 6 and ipone 7 running on iOS 10.2, iMac sierra iOS 10.12, WIndows 10.0, etc </li>
-					<li><strong>Needs:</strong>Steven Morrissey owns a successful custom build bike shop. But he wants to start a Custom Bike online magazine. he wants a site started but does not know exactly how. So he is looking for a web developer that can develop a website but he wants a developer that shares his interest in bicycles so it will be eiser to communicate and understand what exactly he wants. He is referenced to my portfolio page and enjoys what he reads, he than contacts me.  </li>
-				</ul>
+
+				<h3>Persona Info</h3>
+					<ul>
+						<li><strong>Name:</strong>Steven Morrissey
+						</li>
+						<li><strong>Age:</strong>32
+						</li>
+						<li><strong>Job:</strong>Seven Morrissey is successful custom bike shop entrepreneur who wants to start a custom bicycle online magazine that can be converted to an app for convenient viewing.
+						</li>
+						<li><strong>Technology:</strong>Morrissey wants his online magazine to be compatible with Ipone 6 and ipone 7 running on iOS 10.2, iMac sierra iOS 10.12, WIndows 10.0, etc.
+						</li>
+						<li><strong>Needs:</strong>Steven Morrissey owns a successful custom build bike shop. But he wants to start a Custom Bike online magazine. he wants a site started but does not know exactly how. So he is looking for a web developer that can develop a website but he wants a developer that shares his interest in bicycles so it will be easy to communicate and understand what exactly he wants. He is referenced to my portfolio page and enjoys what he reads, he than contacts me.
+						</li>
+					</ul>
+<!-- this next section is the persona case use.-->
+			<h3>Case Use</h3>
+				<p>
+					Steven Morrissey has been up late at night researching web developers to help him bring his idea to the web. Morrissey has a passion for bicycles and custom built bicycles, so he wants to create a magazine based on his passion so he can help organize and find others that have the same passion.
+				</p>
+				<p>
+					He skims through different portfolios of web developers but he does not seem to interested in them or he does not know if they will know or have an idea of how and what content Morrissey want to include in his web magazine. He goes onto his Linkd in account and finds my portfolio.
+				</p>
+				<p>He sees on my portfolio that have a similar interest in bicycles. He clicks on my link that leads him to my web portfolio to see the projects that I have previously worked on. he decides to contact me through the email that is shown on my contact page.</p>
 		</main>
 	</body>
 </html>
