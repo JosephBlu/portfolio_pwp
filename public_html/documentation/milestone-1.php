b0df348d06ddfa3fb55a0349fd0f238d8d796862
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-22 18:14:36 -0700

    added git and php tool set. added Purpose, Audience, and Goals text.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Joseph Ramirez Portfolio Page</title>
+	</head>
+
+	<!-- this will contain the Purpose, Audience, and Goal of the milestone 1 project-->
+	<body>
+		<header>
+			<h1>Joseph Ramirez Portfolio Page</h1>
+		</header>
+		<main>
+			<h2>Purpose</h2>
+				<p>I want my personal web page project to showcase my skills as a full stack web page developer. what i hope to accomplish with my PWP is that my page will attract, primarily, employers but also freelance clients. my page will display my knowledge and experience and give viewers the neccassary confidence to hire me to help build their future.</p>
+
+			<h2>Audience</h2>
+				<p>The portfolio page will primarly target future employers. But i also would like to target local business owners that want to stray away from facebook and want to create a web page for their business or community organizations.</p>
+
+			<h2>Goal</h2>
+				<p> my goal are to:</p>
+					<ul>
+						<li>Aquire a full-time job.</li>
+						<li>Attract freelance clients.</li>
+						<li>gain credibility as a web developer.</li>
+						<li>continually build and improve my skills as a developer.</li>
+					</ul>
+		</main>
+	</body>
+</html>
