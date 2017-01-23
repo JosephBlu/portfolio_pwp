520f455a02c823de1a99a458908f11319e27b6d1
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-22 19:43:40 -0700

    spell check completed

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,12 +16,12 @@
 				<p>I want my personal web page project to showcase my skills as a full stack web page developer. what i hope to accomplish with my PWP is that my page will attract, primarily, employers but also freelance clients. my page will display my knowledge and experience and give viewers the neccassary confidence to hire me to help build their future.</p>
 
 			<h3>Audience</h3>
-				<p>The portfolio page will primarly target future employers. But i also would like to target local business owners that want to stray away from facebook and want to create a web page for their business or community organizations.</p>
+				<p>The portfolio page will primarily targeted future employers. But i also would like to target local business owners that want to stray away from facebook and want to create a web page for their business or community organizations.</p>
 
 			<h3>Goal</h3>
 				<p> my goal are to:</p>
 					<ul>
-						<li>Aquire a full-time job.</li>
+						<li>Acquire a full-time job.</li>
 						<li>Attract freelance clients.</li>
 						<li>gain credibility as a web developer.</li>
 						<li>continually build and improve my skills as a developer.</li>
@@ -38,7 +38,7 @@
 						</li>
 						<li><strong>Job:</strong>Seven Morrissey is successful custom bike shop entrepreneur who wants to start a custom bicycle online magazine that can be converted to an app for convenient viewing.
 						</li>
-						<li><strong>Technology:</strong>Morrissey wants his online magazine to be compatible with Ipone 6 and ipone 7 running on iOS 10.2, iMac sierra iOS 10.12, WIndows 10.0, etc.
+						<li><strong>Technology:</strong>Morrissey wants his online magazine to be compatible with Ipone 6 and iphone 7 running on iOS 10.2, iMac sierra iOS 10.12, Windows 10.0, etc.
 						</li>
 						<li><strong>Needs:</strong>Steven Morrissey owns a successful custom build bike shop. But he wants to start a Custom Bike online magazine. he wants a site started but does not know exactly how. So he is looking for a web developer that can develop a website but he wants a developer that shares his interest in bicycles so it will be easy to communicate and understand what exactly he wants. He is referenced to my portfolio page and enjoys what he reads, he than contacts me.
 						</li>
