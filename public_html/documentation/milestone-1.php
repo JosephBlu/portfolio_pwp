6b1af553355d23fe2af5f3e9ae919fa8fa93a110
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-22 19:17:45 -0700

    added a persona section. and configured deployment.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,13 +11,14 @@
 			<h1>Joseph Ramirez Portfolio Page</h1>
 		</header>
 		<main>
-			<h2>Purpose</h2>
+			<h2>Purpose, Audience, and Goals</h2>
+			<h3>Purpose</h3>
 				<p>I want my personal web page project to showcase my skills as a full stack web page developer. what i hope to accomplish with my PWP is that my page will attract, primarily, employers but also freelance clients. my page will display my knowledge and experience and give viewers the neccassary confidence to hire me to help build their future.</p>
 
-			<h2>Audience</h2>
+			<h3>Audience</h3>
 				<p>The portfolio page will primarly target future employers. But i also would like to target local business owners that want to stray away from facebook and want to create a web page for their business or community organizations.</p>
 
-			<h2>Goal</h2>
+			<h3>Goal</h3>
 				<p> my goal are to:</p>
 					<ul>
 						<li>Aquire a full-time job.</li>
@@ -25,6 +26,16 @@
 						<li>gain credibility as a web developer.</li>
 						<li>continually build and improve my skills as a developer.</li>
 					</ul>
+
+			<!-- The following will be the persona information. the first part will contain Name, Age, Job/Role, Technology, and Needs. -->
+			<h2>Persona</h2>
+				<ul>
+					<li><strong>Name:</strong>Steven Morrissey</li>
+					<li><strong>Age:</strong>32</li>
+					<li><strong>Job:</strong>Seven Morrissey is successful custom bike shop entrepreneur who wants to start a custom bicycle online magazine that can be converted to an app for convenient viewing.</li>
+					<li><strong>Technology:</strong>Morrissey wants his online magazine to be compatible with Ipone 6 and ipone 7 running on iOS 10.2, iMac sierra iOS 10.12, WIndows 10.0, etc </li>
+					<li><strong>Needs:</strong>Steven Morrissey owns a successful custom build bike shop. But he wants to start a Custom Bike online magazine. he wants a site started but does not know exactly how. So he is looking for a web developer that can develop a website but he wants a developer that shares his interest in bicycles so it will be eiser to communicate and understand what exactly he wants. He is referenced to my portfolio page and enjoys what he reads, he than contacts me.  </li>
+				</ul>
 		</main>
 	</body>
 </html>
