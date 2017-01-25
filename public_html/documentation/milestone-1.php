e6e651cc18db948cd935808a6f9f06c0700c4007
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-25 11:48:25 -0700

    fixed indentation on h3 and p

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,46 +13,45 @@
 		<main>
 			<h2>Purpose, Audience, and Goals</h2>
 			<h3>Purpose</h3>
-				<p>I want my personal web page project to showcase my skills as a full stack web page developer. what i hope to accomplish with my PWP is that my page will attract, primarily, employers but also freelance clients. my page will display my knowledge and experience and give viewers the neccassary confidence to hire me to help build their future.</p>
+			<p>I want my personal web page project to showcase my skills as a full stack web page developer. what i hope to accomplish with my PWP is that my page will attract, primarily, employers but also freelance clients. my page will display my knowledge and experience and give viewers the neccassary confidence to hire me to help build their future.</p>
 
 			<h3>Audience</h3>
-				<p>The portfolio page will primarily targeted future employers. But i also would like to target local business owners that want to stray away from facebook and want to create a web page for their business or community organizations.</p>
+			<p>The portfolio page will primarily targeted future employers. But i also would like to target local business owners that want to stray away from facebook and want to create a web page for their business or community organizations.</p>
 
 			<h3>Goal</h3>
-				<p> my goal are to:</p>
-					<ul>
-						<li>Acquire a full-time job.</li>
-						<li>Attract freelance clients.</li>
-						<li>gain credibility as a web developer.</li>
-						<li>continually build and improve my skills as a developer.</li>
+			<p> my goal are to:</p>
+				<ul>
+					<li>Acquire a full-time job.</li>
+					<li>Attract freelance clients.</li>
+					<li>gain credibility as a web developer.</li>
+					<li>continually build and improve my skills as a developer.</li>
 					</ul>
 
 			<!-- The following will be the persona information. the first part will contain Name, Age, Job/Role, Technology, and Needs. -->
 			<h2>Persona</h2>
 
 				<h3>Persona Info</h3>
-					<ul>
-						<li><strong>Name:</strong>Steven Morrissey
-						</li>
-						<li><strong>Age:</strong>32
-						</li>
-						<li><strong>Job:</strong>Seven Morrissey is successful custom bike shop entrepreneur who wants to start a custom bicycle online magazine that can be converted to an app for convenient viewing.
-						</li>
-						<li><strong>Technology:</strong>Morrissey wants his online magazine to be compatible with Ipone 6 and iphone 7 running on iOS 10.2, iMac sierra iOS 10.12, Windows 10.0, etc.
-						</li>
-						<li><strong>Needs:</strong>Steven Morrissey owns a successful custom build bike shop. But he wants to start a Custom Bike online magazine. he wants a site started but does not know exactly how. So he is looking for a web developer that can develop a website but he wants a developer that shares his interest in bicycles so it will be easy to communicate and understand what exactly he wants. He is referenced to my portfolio page and enjoys what he reads, he than contacts me.
-						</li>
-					</ul>
+				<ul>
+					<li><strong>Name:</strong>Steven Morrissey
+					</li>
+					<li><strong>Age:</strong>32
+					</li>
+					<li><strong>Job:</strong>Seven Morrissey is successful custom bike shop entrepreneur who wants to start a custom bicycle online magazine that can be converted to an app for convenient viewing.
+					</li>
+					<li><strong>Technology:</strong>Morrissey wants his online magazine to be compatible with Ipone 6 and iphone 7 running on iOS 10.2, iMac sierra iOS 10.12, Windows 10.0, etc.
+					</li>
+					<li><strong>Needs:</strong>Steven Morrissey owns a successful custom build bike shop. But he wants to start a Custom Bike online magazine. he wants a site started but does not know exactly how. So he is looking for a web developer that can develop a website but he wants a developer that shares his interest in bicycles so it will be easy to communicate and understand what exactly he wants. He is referenced to my portfolio page and enjoys what he reads, he than contacts me.
+					</li>
+				</ul>
 <!-- this next section is the persona case use.-->
 			<h3>Case Use</h3>
-				<p>
+			<p>
 					Steven Morrissey has been up late at night researching web developers to help him bring his idea to the web. Morrissey has a passion for bicycles and custom built bicycles, so he wants to create a magazine based on his passion so he can help organize and find others that have the same passion.
-				</p>
-				<p>
-					He skims through different portfolios of web developers but he does not seem to interested in them or he does not know if they will know or have an idea of how and what content Morrissey want to include in his web magazine. He goes onto his Linkd in account and finds my portfolio.
-				</p>
-				<p>He sees on my portfolio that have a similar interest in bicycles. He clicks on my link that leads him to my web portfolio to see the projects that I have previously worked on. he decides to contact me through the email that is shown on my contact page.
-				</p>
+			</p>
+			<p>He skims through different portfolios of web developers but he does not seem to interested in them or he does not know if they will know or have an idea of how and what content Morrissey want to include in his web magazine. He goes onto his Linkd in account and finds my portfolio.
+			</p>
+			<p>He sees on my portfolio that have a similar interest in bicycles. He clicks on my link that leads him to my web portfolio to see the projects that I have previously worked on. he decides to contact me through the email that is shown on my contact page.
+			</p>
 		</main>
 	</body>
 </html>
