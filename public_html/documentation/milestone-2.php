4d7a1ec2a563ab4754d1038acc3d049e925edd00
Joseph Ramirez <jramirez98@cnm.edu>
2017-02-28 11:32:43 -0700

    added the index.php and the structure for the final milestone

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -16,24 +16,24 @@
 			</p>
 		<!-- these following images are the wireframe for the web and mobile page for the portfolio-->
 		<h2>Web Page Wire Frame</h2>
-		<img src="../images/webpage/homepage.png" alt="homepage" width="500" height="400">
-		<img src="../images/deviceFormat/homepageDv.png" alt="homepageDv" width="250" height="400">
+		<img src="images/webpage/homepage.png" alt="homepage" width="500" height="400">
+		<img src="images/deviceFormat/homepageDv.png" alt="homepageDv" width="250" height="400">
 		<p>
 			On the homepage, I will have a brief about section, an introduction explaining about what they are going to see in the upcoming pages. The navigation bar, the bar will be present through-out the scrolling of the page to give the viewer easy access to help them get around the page. I believe the nav bar will be useful as I slowly start to build my portfolio.
 		</p>
-		<img src="../images/webpage/project.png" alt="project" width="500" height="400">
-		<img src="../images/deviceFormat/projectDv.png" alt="projectDv" width="250" height="400">
-		<img src="../images/deviceFormat/projectPgTwoDv.png" alt="projectPgTwoD" width="250" height="400">
+		<img src="images/webpage/project.png" alt="project" width="500" height="400">
+		<img src="images/deviceFormat/projectDv.png" alt="projectDv" width="250" height="400">
+		<img src="images/deviceFormat/projectPgTwoDv.png" alt="projectPgTwoD" width="250" height="400">
 		<p>
 			On the project page, there will be a display of my past and current projects that I have either collaborated or individually created. Next to the images will be a brief description of what the projects are about and what i used to create the projects. However, on the mobile site, to prevent from clustering the screen, I have created a individual page for each project.
 		</p>
-		<img src="../images/webpage/skills.png" alt="skills" width="500" height="400">
-		<img src="../images/deviceFormat/skillsDv.png" alt="skillsDv" width="250" height="400">
+		<img src="images/webpage/skills.png" alt="skills" width="500" height="400">
+		<img src="images/deviceFormat/skillsDv.png" alt="skillsDv" width="250" height="400">
 		<p>
 			The skills page will be dedicated to the technology and software that I have experienced using before and during Deeps Dive Bootcamp.
 		</p>
-		<img src="../images/webpage/contact.png" alt="contact" width="500" height="400">
-		<img src="../images/deviceFormat/contactDv.png" alt="contactDv" width="250" height="400">
+		<img src="images/webpage/contact.png" alt="contact" width="500" height="400">
+		<img src="images/deviceFormat/contactDv.png" alt="contactDv" width="250" height="400">
 		<p>
 			On the contact page, I will keep it simple by displaying my email address.
 		</p>
