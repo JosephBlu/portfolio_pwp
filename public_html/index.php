dc1a025c82eb28f9db8a8dfa4de3372feac10de7
Joseph Ramirez <jramirez98@cnm.edu>
2017-02-28 16:59:58 -0700

    added content to PWP

---
+++ public_html/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<meta name="viewport" content="width=device width, initial_scale=1.0"/>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!--Font awesome-->
+		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<title>Joseph Ramirez</title>
+	</head>
+
+	<body>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>Joseph Ramirez</h1>
+					<h2>Welcome to My Portfolio</h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<p>I am a entry-level, Albuquerque-based Full Stack Web Developer. I am an ambitious,  currently Freelancing and looking foreword to meet new clients and start new projects.</p>
+					<p> I have experience in using Php, CSS, Bootstrap, mySQL, Wordpress, and Xcoder.  </p>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<h2>PROJECTS</h2>
+			<div class="row">
+				<div class="col-md-6">
+					<h3>GigHub</h3>
+					<p>GigHub is a social Networking site focused on helping musicians, venues, and fans to network with one another. This site presents a opportunity for musicians to be discovered, helps venues discover and contact new bands or local favorites to perform at the venues events, and allows fans to stay in touch and to know where and when their favorite band will be playing.</p>
+					<p>This project was a group capstone project that was created to graduate from Deep Dive Coding Bootcamp. The languages we used for the back-end was PHP for the coding and mySQL to create the database to store information. the Front-end languages we used are CSS, Bootstrap, and Angular2.</p>
+				</div>
+					<div class="col-md-6">
+						<h3>Blu Bird Tile Art</h3>
+						<p>Blu Bird Tile Art is a Albuquerque Local Business that specializes in hand-crafting tiles for basic home and business back-splashes and also creates private and public funded Mosaic art installations. </p>
+						<p> The Front-end of this project was created using Wordpress Content Managment Sytem. Also, for this project, I personally photographed the images using a Nikon D3300 and Adobe Lightroom for minor editing. </p>
+					</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<H2>SKILLS</H2>
+			<div class="row">
+				<div class="col-md-4">
+					<h3>Back-End</h3>
+					<p>For the Back-end, my skill set consists of PhP, HTML, mySQL, and Xcoder. </p>
+				</div>
+				<div class="col-md-4">
+					<h3>Front-End</h3>
+					<p>The Front-end languages that i have experiencein using are JavaScript, BootStrap, Angular2, and CSS. </p>
+				</div>
+				<div class="col-md-4">
+					<h3>Wordpress</h3>
+					<p>For projects that do not need me to get my hands dirty. I also have experience in creating or designing websites using Wordpress. </p>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4">
+
+
+				</div>
+				<div class="col-md-offset-4">
+					<h2> Feel free to Contact Me!</h2>
+			</div>
+		</div>
+	</body>
+
+</html>
