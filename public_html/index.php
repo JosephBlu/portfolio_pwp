d43183f47fb8217a915538493faa561211de425b
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-02 09:34:37 -0700

    added **/php/mail-config.php to git ignore

--- public_html/index.php
+++ public_html/index.php
@@ -13,22 +13,45 @@
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
+		<!-- jQuery Form, Validate, Additional Methods -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js" type="text/javascript"></script>
+
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="form-validate.js"></script>
+
+		<!-- recaptcha google-->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<title>Joseph Ramirez</title>
 	</head>
 
 	<body>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-12">
-					<h1>Joseph Ramirez</h1>
-					<h2>Welcome to My Portfolio</h2>
+
+		<nav class="navbar navbar-default navbar-fixed-bottom">
+			<div class="container">
+				<div class="btn-group btn-group-justified">
+					<a href="#" class="btn btn-default">HOME</a>
+					<a href="#" class="btn btn-default">PROJECTS</a>
+					<a href="#" class="btn btn-default">SKILLS</a>
+					<a href="#" class="btn btn-default">CONTACT</a>
 				</div>
 			</div>
-		</div>
+		</nav>
 
-		<div class="container-fluid">
+		<div class="jumbotron">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h1>Joseph Ramirez</h1>
+						<h2>Welcome to My Portfolio</h2>
+					</div>
+				</div>
+			</div>
+		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 					<p>I am a entry-level, Albuquerque-based Full Stack Web Developer. I am an ambitious,  currently Freelancing and looking foreword to meet new clients and start new projects.</p>
@@ -36,8 +59,9 @@
 				</div>
 			</div>
 		</div>
+		</div>
 
-		<div class="container-fluid">
+		<div class="container">
 			<h2>PROJECTS</h2>
 			<div class="row">
 				<div class="col-md-6">
@@ -46,13 +70,17 @@
 					<p>This project was a group capstone project that was created to graduate from Deep Dive Coding Bootcamp. The languages we used for the back-end was PHP for the coding and mySQL to create the database to store information. the Front-end languages we used are CSS, Bootstrap, and Angular2.</p>
 				</div>
 					<div class="col-md-6">
+						<a href="#" class="thumbnail">
+							<img src="images" alt="bluBird-16x16.png">
+						</a>
 						<h3>Blu Bird Tile Art</h3>
 						<p>Blu Bird Tile Art is a Albuquerque Local Business that specializes in hand-crafting tiles for basic home and business back-splashes and also creates private and public funded Mosaic art installations. </p>
 						<p> The Front-end of this project was created using Wordpress Content Managment Sytem. Also, for this project, I personally photographed the images using a Nikon D3300 and Adobe Lightroom for minor editing. </p>
 					</div>
 			</div>
 		</div>
-		<div class="container-fluid">
+
+		<div class="container">
 			<H2>SKILLS</H2>
 			<div class="row">
 				<div class="col-md-4">
@@ -70,14 +98,67 @@
 			</div>
 		</div>
 
-		<div class="container-fluid">
+
+		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
 
 
 				</div>
-				<div class="col-md-offset-4">
+				<div class="col-md-4">
 					<h2> Feel free to Contact Me!</h2>
+					<!--Begin Contact Form-->
+					<form id="contact-form" action="php/mailer.php" method="post">
+						<div class="form-group">
+							<label for="name">Name <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-user" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="email">Email <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject</label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="message">Message <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-comment" aria-hidden="true"></i>
+								</div>
+								<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+							</div>
+						</div>
+
+						<!-- reCAPTCHA -->
+						<div class="g-recaptcha" data-sitekey="6LescRcUAAAAAHN6D0L3YmfxMJlmZoMcu0WUSuQ4"></div>
+
+						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+					</form>
+
+					<!--empty area for form error/success output-->
+					<div class="row">
+						<div class="col-xs-12">
+							<div id="output-area"></div>
+						</div>
+					</div>
 			</div>
 		</div>
 	</body>
