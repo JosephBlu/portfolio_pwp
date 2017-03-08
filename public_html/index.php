d29d9d6de97dad147e581e5ac1e30bad73019b8a
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-08 07:09:59 -0700

    add my skills section

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-		<meta name="viewport" content="width=device- width, initial_scale=1.0"/>
+		<meta name="viewport" content="width=device- width, initial_scale=1"/>
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!--Font awesome-->
@@ -23,6 +23,7 @@
 
 		<!-- google fonts-->
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400i,600,700|Roboto+Mono:100,400,500,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Orbitron:500" rel="stylesheet">
 
 		<!--favicon generator-->
 		<meta name="msapplication-TileColor" content="#ffffff">
@@ -42,8 +43,7 @@
 	<body>
 		<div class="padding-large" id="main">
 			<header class="container">
-				<img alt="Brand"  class="img-circle" src="./images/beard.png" height="150" width="125">
-				<h1>I'm Joseph Ramirez</h1>
+				<h1>Joseph Ramirez</h1>
 				<p> Full Stack Web Developer</p>
 				<img src="./images/IMG_3346.jpg" width="900" height="700" align="center">
 			</header>
@@ -55,9 +55,10 @@
 					<div class="col-md-12">
 						<h2>About Me</h2>
 					</div>
+					<img alt="Brand"  class="img-circle" src="./images/beard.png" height="150" width="125">
 					<div class="col-md-12">
-						<p>I am a entry-level, Albuquerque-based Full Stack Web Developer. I am an ambitious,  currently Freelancing and looking foreword to meet new clients and start new projects.</p>
-						<p>I have experience in using Php, CSS, Bootstrap, mySQL, Wordpress, and Xcoder.</p>
+						<p>Hello! I am a entry-level, Albuquerque-based freelance Full Stack Web Developer. I am motivated in creating the best quality of web sites and I am eager to learn new skills in the process. I also like to practice in writing clean or dry code, to allow better laoding time and easy to maitain. </p>
+						<p>I curently have experience in using Php, CSS, Bootstrap, mySQL, Wordpress, and Xcoder. But i am always seeking in learning new languages and frameworks so i can provide a varity of service to my employers and my clients. </p>
 					</div>
 				</div>
 			</div>
@@ -71,16 +72,41 @@
 						</i>
 					</div>
 					<div class="col-md-6 ">
-
+						<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="GigHub is a social Networking site focused on helping musicians, venues, and fans to network with one another. This site presents a opportunity for musicians to be discovered, helps venues discover and contact new bands or local favorites to perform at the venues events, and allows fans to stay in touch and to know where and when their favorite band will be playing.
+					This project was a group capstone project that was created to graduate from Deep Dive Coding Bootcamp. The languages we used for the back-end was PHP for the coding and mySQL to create the database to store information. the Front-end languages we used are CSS, Bootstrap, and Angular2.. ">
 							<p>GigHub</p>
+						</button>
 					</div>
 					<div class="col-md-6 ">
 						<img src="images/blubird1.png" width="325" height="400">
+						<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="<p>Blu Bird Tile Art is a Albuquerque Local Business that specializes in hand-crafting tiles for basic home and business back-splashes and also creates private and public funded Mosaic art installations.The Front-end of this project was created using Wordpress Content Managment Sytem. Also, for this project, I personally photographed the images using a Nikon D3300 and Adobe Lightroom for minor editing. ">
 							<p>Blu Bird Tile Art</p>
+						</button>
 					</div>
 				</div>
 			</div>
 
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>SKILLS</h2>
+				</div>
+				<div class="row">
+				<div col-md-4>
+					<h3>Back-End</h3>
+					<p>My current back-end languages consist of PHP, HTML, mySql, and Xcode. The two languages and frameworks I am in the process of learning are Python and Laravel.  </p>
+				</div>
+					<div col-md-4>
+						<h3>Front-End</h3>
+						<p>For the front-end, I am experienced in CSS, Angular2, and Bootstrap. These three languages I am constantly learning new ways to deploy and strengthen my understanding of how to use them effectivly.   </p>
+					</div>
+					<div col-md-4>
+						<h3>Wordpress</h3>
+						<p>Wordpress has been a valuable tool to learn, especially for clients who are wanting to update the contents of thier website themselves. However, setting up and design can be tedious, time consuming, and overwhelming. But I am here to help. Learning wordpress is how I started my journey as a Web Developer </p>
+					</div>
+			</div>
+			</div>
+		</div>
 		<!-- this is the contact Page-->
 	<div class="jumbotron-contact">
 		<div class="container">
@@ -89,6 +115,7 @@
 				</div>
 				<div class="col-md-4">
 					<h2> Feel free to Contact Me!</h2>
+
 					<!--Begin Contact Form-->
 					<form id="contact-form" action="php/mailer.php" method="post">
 						<div class="form-group">
