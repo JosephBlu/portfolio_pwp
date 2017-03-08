c9b18950ba9a81c58f7b779a5a942cdeff87ede6
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-07 20:28:06 -0700

    restructured portfolio page

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-		<meta name="viewport" content="width=device width, initial_scale=1.0"/>
+		<meta name="viewport" content="width=device- width, initial_scale=1.0"/>
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!--Font awesome-->
@@ -38,78 +38,55 @@
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<title>Joseph Ramirez</title>
 	</head>
-
+	<!-- This is for the first section-->
 	<body>
-		<nav class="navbar navbar-default navbar-fixed-bottom">
-			<div class="container-nav">
-				<div class="btn-group btn-group-justified">
-					<a href="#" class="btn btn-default">HOME</a>
-					<a href="#" class="btn btn-default">PROJECTS</a>
-					<a href="#" class="btn btn-default">SKILLS</a>
-					<a href="#" class="btn btn-default">CONTACT</a>
-				</div>
-			</div>
-		</nav>
+		<div class="padding-large" id="main">
+			<header class="container">
+				<img alt="Brand"  class="img-circle" src="./images/beard.png" height="150" width="125">
+				<h1>I'm Joseph Ramirez</h1>
+				<p> Full Stack Web Developer</p>
+				<img src="./images/IMG_3346.jpg" width="900" height="700" align="center">
+			</header>
+		</div>
 
-		<div class="jumbotron">
-		<header class="cover-header">
-			<div class="container">
-				  <div class="row">
-					  <div class="col-md-12">
-						  <h1>Welcome to My Portfolio!</h1>
-						  <p>I am a entry-level, Albuquerque-based Full Stack Web Developer. I am an ambitious,  currently Freelancing and looking foreword to meet new clients and start new projects. <br>I have experience in using Php, CSS, Bootstrap, mySQL, Wordpress, and Xcoder.</p>
-					  </div>
-				  </div>
-			</div>
-		</header>
-	</div>
-
-		<div class="jumbotron-project">
-		<div class="container">
-			<h2>PROJECTS</h2>
-			<div class="row">
-				<div class="col-md-6">
-					<h3>GigHub</h3>
-					<p>GigHub is a social Networking site focused on helping musicians, venues, and fans to network with one another. This site presents a opportunity for musicians to be discovered, helps venues discover and contact new bands or local favorites to perform at the venues events, and allows fans to stay in touch and to know where and when their favorite band will be playing.</p>
-					<p>This project was a group capstone project that was created to graduate from Deep Dive Coding Bootcamp. The languages we used for the back-end was PHP for the coding and mySQL to create the database to store information. the Front-end languages we used are CSS, Bootstrap, and Angular2.</p>
-				</div>
-				<div class="col-xs-6 col-md-3">
-					<a href="#" class="thumbnail">
-						</a>
-						<h3>Blu Bird Tile Art</h3>
-						<p>Blu Bird Tile Art is a Albuquerque Local Business that specializes in hand-crafting tiles for basic home and business back-splashes and also creates private and public funded Mosaic art installations. </p>
-						<p> The Front-end of this project was created using Wordpress Content Managment Sytem. Also, for this project, I personally photographed the images using a Nikon D3300 and Adobe Lightroom for minor editing. </p>
+		<!-- this is the About page -->
+			<div class="container" id="aboutMe">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>About Me</h2>
 					</div>
-			</div>
-		</div>
-		</div>
-		<div class="jumbotron-skill">
-		<div class="container">
-			<H2>SKILLS</H2>
-			<div class="row">
-				<div class="col-md-4">
-					<h3>Back-End</h3>
-					<p>For the Back-end, my skill set consists of PhP, HTML, mySQL, and Xcoder. </p>
-				</div>
-				<div class="col-md-4">
-					<h3>Front-End</h3>
-					<p>The Front-end languages that i have experiencein using are JavaScript, BootStrap, Angular2, and CSS. </p>
-				</div>
-				<div class="col-md-4">
-					<h3>Wordpress</h3>
-					<p>For projects that do not need me to get my hands dirty. I also have experience in creating or designing websites using Wordpress. </p>
+					<div class="col-md-12">
+						<p>I am a entry-level, Albuquerque-based Full Stack Web Developer. I am an ambitious,  currently Freelancing and looking foreword to meet new clients and start new projects.</p>
+						<p>I have experience in using Php, CSS, Bootstrap, mySQL, Wordpress, and Xcoder.</p>
+					</div>
 				</div>
 			</div>
-		</div>
-		</div>
 
+		<!--this is for the projects section-->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<i class="fa fa-code fa-lg" aria-hidden="true">
+						<h2> PROJECTS</h2>
+						</i>
+					</div>
+					<div class="col-md-6 ">
+
+							<p>GigHub</p>
+					</div>
+					<div class="col-md-6 ">
+						<img src="images/blubird1.png" width="325" height="400">
+							<p>Blu Bird Tile Art</p>
+					</div>
+				</div>
+			</div>
+
+		<!-- this is the contact Page-->
 	<div class="jumbotron-contact">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
-
 				</div>
-
 				<div class="col-md-4">
 					<h2> Feel free to Contact Me!</h2>
 					<!--Begin Contact Form-->
@@ -170,5 +147,12 @@
 	</div>
 	</div>
 	</body class="font">
+
+	<script>
+
+		$(function () {
+			$('[data-toggle="popover"]').popover()
+		})
+	</script>
 
 </html>
