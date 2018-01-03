d1d30cffa2fbf0a3382d12e3ed98a68b12a6d2a7
Joseph Ramirez <jramirez98@cnm.edu>
2018-01-03 09:26:49 -0700

    aded the custom-javascript file withing the js dir and the contents inside the custome-javascript file.

--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -3,6 +3,7 @@
  * Get the relative path.
  * @see https://raw.githubusercontent.com/kingscreations/farm-to-you/master/php/lib/_header.php FarmToYou Header
  **/
+
 // include the appropriate number of dirname() functions
 // on line 8 to correctly resolve your directory's path
 require_once(dirname(dirname(__DIR__)) . "/root-path.php");
@@ -17,10 +18,9 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial_scale=1"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!--Font awesome-->
 		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
@@ -30,15 +30,18 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 		<link rel="stylesheet" href="<?php echo $PREFIX;?>css/utilities.css" type="text/css"/>
 		<link rel="stylesheet" href="<?php echo $PREFIX;?>css/theme.css" type="text/css"/>
 
+		<!-- jQuery (required for Bootstap's JS plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+		<!-- Bootstrap: Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 
-
-		<!-- jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
 
 		<!-- jQuery Form, Validate, Additional Methods -->
@@ -58,21 +61,17 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 		<script src="<?php echo $PREFIX;?>js/custom-javascript.js" type="text/javascript"></script>
 
 <!--		 google fonts-->
-		<link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Roboto+Condensed:400,400i|Roboto+Slab|Roboto:400,400i" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Roboto+Condensed:300,300i,400,400i|Roboto+Slab:100,300,400|Roboto:300,300i,400,400i" rel="stylesheet">
 
 		<!--favicon generator-->
 		<meta name="msapplication-TileColor" content="#ffffff">
-
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
 
 		<!-- recaptcha google-->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
-		<title>Joseph Ramirez</title>
+
+		<!-- echo the $PAGE_TITLE variable here -->
+		<title><?php echo $PAGE_TITLE;?></title>
 	</head>
